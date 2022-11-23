0209646f345b5720d8cd2c7fe9c4b5c6229c0840
manzar <newlaptoppak@gmail.com>
2022-11-23 10:07:34 +0500

    updating the package.json

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/',user_Route);
 
 
 // Port
-const port =process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
